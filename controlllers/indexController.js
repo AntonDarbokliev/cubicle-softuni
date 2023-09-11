9c15811efa3c2b849e8bbd4e2a12289c84fa8b45
Anton <georgedarbokliev@gmail.com>
2023-09-11 22:51:30 +0300

    Added a search function

--- controlllers/indexController.js
+++ controlllers/indexController.js
@@ -1,28 +1,48 @@
-const { getAll, getById } = require('../services/cubeService.js');
+const { getAll, getById } = require("../services/cubeService.js");
+
+const router = require("express").Router();
+
+router.get("/", (req, res) => {
+  const cubes = getAll();
+  console.log(req.query);
+  res.render("index", {
+    title: "Browse",
+    cubes,
+  });
+});
+
+router.get("/filter", (req, res) => {
+  const cubes = getAll();
+  const searchValue = req.query.search;
+  const fromValue = req.query.from;
+  const toValue = req.query.to;
+  let filtered = [];
+  if (searchValue || toValue || fromValue) {
+    filtered = cubes.filter((el) => {
+      if (el.name.toLowerCase() === searchValue.toLowerCase()) return true;
+      else if (el.difficultyLevel >= fromValue && el.difficultyLevel <= toValue)return true;
+      return false;
+    });
 
 
-const router = require('express').Router();
+  }
 
-router.get('/',(req,res)=>{
-    const cubes = getAll()
-    res.render('index',{
-        title: "Browse",
-        cubes
-    })
-})
+  res.render("index", {
+    title: "Browse",
+    cubes,
+    filtered,
+  });
+});
 
-
-router.get('/details/:id',(req,res)=>{
-    const id = req.params.id
-    const cube = getById(id)
-    console.log(id);
-    console.log(cube);
-    if(cube){
-        res.render('details',{
-            cube
-        })
-    }else{
-        res.render('404')
-    }
-})
-module.exports = router
+router.get("/details/:id", (req, res) => {
+  const id = req.params.id;
+  const cube = getById(id);
+  if (cube) {
+    res.render("details", {
+      cube,
+    });
+  } else {
+    res.render("404");
+  }
+});
+module.exports = router;
